fdfe0e25ea49eca79c6a58ed998994eb9706d4a4
felipetodev <fe.ossandon.u@gmail.com>
2023-10-16 20:20:23 -0300

    feat: add timer selector component

--- src/app/components/home.tsx
+++ src/app/components/home.tsx
@@ -1,12 +1,14 @@
 'use client'
 
-import Timer from './timer'
+import TimerSelector from './timer-selector'
 
 function Home () {
   return (
     <main className="grid place-content-center h-screen">
       <h1 className="text-4xl">Pomodoro App</h1>
-      <Timer />
+      <div className='bg-red-500/50 p-4 rounded-xl'>
+        <TimerSelector />
+      </div>
     </main>
   )
 }
