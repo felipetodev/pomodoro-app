60c531edfffe2c2615fdacf39debd7da7149a544
felipetodev <fe.ossandon.u@gmail.com>
2023-10-16 21:24:38 -0300

    feat: add circular progress bar

---
+++ src/app/components/circular-progress.tsx
@@ -0,0 +1,30 @@
+import {
+  CircularProgressbarWithChildren as CircularProgressbar,
+  buildStyles
+} from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+import { invertTimeValue } from '../lib/utils'
+
+interface Props {
+  time: number
+  maxValue: number
+  children: React.ReactNode
+}
+
+function CircularProgress ({ time, maxValue, children }: Props) {
+  return (
+    <div className='w-72 h-72 mx-auto my-10'>
+      <CircularProgressbar
+        styles={buildStyles({
+          pathColor: 'hsl(var(--primary))'
+        })}
+        value={invertTimeValue({ time, maxValue })}
+        maxValue={maxValue}
+      >
+        {children}
+      </CircularProgressbar>
+    </div>
+  )
+}
+
+export default CircularProgress
