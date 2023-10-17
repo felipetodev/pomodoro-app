9a2464add5d361a8ba25c7fe343534d0c7d6588b
felipetodev <fe.ossandon.u@gmail.com>
2023-10-16 21:25:16 -0300

    feat: add slider component

---
+++ src/app/components/ui/slider.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import * as SliderPrimitive from '@radix-ui/react-slider'
+import cn from 'clsx'
+
+export const Slider = React.forwardRef<
+React.ElementRef<typeof SliderPrimitive.Root>,
+React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <SliderPrimitive.Root
+    ref={ref}
+    className={cn(
+      'relative flex touch-none select-none items-center group',
+      className
+    )}
+    {...props}
+  >
+    <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-gray-800">
+      <SliderPrimitive.Range className="absolute h-full bg-white" />
+    </SliderPrimitive.Track>
+
+  </SliderPrimitive.Root>
+))
+
+Slider.displayName = SliderPrimitive.Root.displayName
